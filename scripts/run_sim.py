3239826995a6966ca758e7cff98c964fa0b27d7e
Adam Coogan <dr.adam.coogan@gmail.com>
2020-02-12 17:19:07 +0100

    cleanup

--- scripts/run_sim.py
+++ scripts/run_sim.py
@@ -1,14 +1,14 @@
+from math import sqrt
+
 import click
+import matplotlib.pyplot as plt
+import numpy as np
+from amuse.datamodel import Particles
 from amuse.io import read_set_from_file, write_set_to_file
 # from amuse.community.ph4.interface import ph4 as Gravity
 from amuse.lab import Hermite as Gravity
 from amuse.units import nbody_system, units
-from amuse.datamodel import Particles
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-import numpy as np
-from math import sqrt
-
 
 
 def print_log(time, gravity):
@@ -20,49 +20,30 @@ def print_log(time, gravity):
     print(f"[time = {time.number:.2f}] E_tot = {E.number}")
 
 
-def plot_positions(smbh_positions, imbh_positions):
-    plt.figure(figsize=(12, 4))
-
-    plt.subplot(1, 3, 1)
-    plt.plot(smbh_positions[:, 0], smbh_positions[:, 1], label="SMBH")
-    plt.plot(imbh_positions[:, 0], imbh_positions[:, 1], label="IMBH")
-    plt.xlabel(r"$x$ (pc)")
-    plt.ylabel(r"$y$ (pc)")
-    plt.legend()
-
-    plt.subplot(1, 3, 2)
-    print(np.sqrt(((smbh_positions - imbh_positions)**2).sum(1)).shape)
-    plt.plot(np.sqrt(((smbh_positions - imbh_positions)**2).sum(1)))
-    plt.xlabel(r"$t$ (s)")
-    plt.ylabel(r"Separation (pc)")
-
-    plt.subplot(1, 3, 3)
-    plt.plot(smbh_positions[:, 2], label="SMBH")
-    plt.plot(imbh_positions[:, 2], label="IMBH")
-    plt.xlabel(r"$t$ (s)")
-    plt.ylabel(r"$z$ (pc)")
-    plt.legend()
-
-    plt.tight_layout()
-    plt.savefig("test.png")
-
-
 @click.command()
 @click.option("--ic_file", default=None, help="initial condition file")
 @click.option("--n_workers", default=1, help="number of workers")
-@click.option("--softening_length", default=1e-15, help="softening length (pc)")
+@click.option("--softening_length", default=1e-10, help="softening length (pc)")
 @click.option("--accuracy_parameter", default=0.1)
 @click.option("--manage_encounters", default=1)
-@click.option("--end_time", default=100.)
+@click.option("--end_time", default=100.0)
 @click.option("--n_steps", default=100)
-@click.option("--snap_dir",default="../snapshots")
-def run(ic_file, n_workers, softening_length, accuracy_parameter,
-        manage_encounters, end_time, n_steps, snap_dir):
-    print(units.constants.G.in_(units.parsec / units.MSun * units.kms**2))
+@click.option("--snap_dir", default="../snapshots")
+def run(
+    ic_file,
+    n_workers,
+    softening_length,  # 1e-15 is a good value; 1e-10 looks questionable
+    accuracy_parameter,
+    manage_encounters,
+    end_time,
+    n_steps,
+    snap_dir,
+):
+    print(units.constants.G.in_(units.parsec / units.MSun * units.kms ** 2))
     end_time = end_time | units.s
     delta_t = end_time / n_steps
 
-    converter = nbody_system.nbody_to_si(1. | units.MSun, 1. | units.kms)
+    converter = nbody_system.nbody_to_si(1.0 | units.MSun, 1.0 | units.kms)
     gravity = Gravity(converter, number_of_workers=n_workers, redirection="none")
     gravity.initialize_code()
     gravity.parameters.set_defaults()
@@ -70,11 +51,11 @@ def run(ic_file, n_workers, softening_length, accuracy_parameter,
 
     # Load initial conditions
     bodies = read_set_from_file(ic_file, "hdf5")
-    print("> Loaded initial conditions")
+    print(f"> Loaded initial conditions for {len(bodies)} particles")
 
     # Set softening parameter
     softening_length = softening_length | units.parsec
-    gravity.parameters.epsilon_squared = softening_length**2
+    gravity.parameters.epsilon_squared = softening_length ** 2
     print(f"> Softening length: {softening_length.number} pc")
 
     gravity.parameters.timestep_parameter = accuracy_parameter
@@ -98,9 +79,6 @@ def run(ic_file, n_workers, softening_length, accuracy_parameter,
     stopping_condition.enable()
     print("> Added stopping condition")
 
-    smbh_positions = []
-    imbh_positions = []
-
     # Run the simulation!
     print(f"Running simulation: {end_time.number} s in {n_steps} steps")
     for i in tqdm(range(n_steps)):
@@ -120,9 +98,6 @@ def run(ic_file, n_workers, softening_length, accuracy_parameter,
 
         # Copy values from the module to the set in memory.
         channel.copy()
-        # Save positions
-        smbh_positions.append(bodies[0].position.number)
-        imbh_positions.append(bodies[1].position.number)
 
         if stopping_condition.is_set():
             body_1 = stopping_condition.particles(0)[0]
@@ -134,26 +109,12 @@ def run(ic_file, n_workers, softening_length, accuracy_parameter,
         if len(bodies) != n_bodies:
             raise ValueError("Number of bodies changed")
 
-        # print_log(time, gravity)
-        
         # Output snapshot of bodies
         bodies.time = time
-        write_set_to_file(bodies, f'{snap_dir}/snapshot_{i}.hdf5','hdf5')
-        
-        
-        
+        write_set_to_file(bodies, f"{snap_dir}/snapshot_{i}.hdf5", "hdf5")
 
     gravity.stop()
     print("> Finished simulation")
-
-    smbh_positions = np.array(smbh_positions)
-    imbh_positions = np.array(imbh_positions)
-
-    plot_positions(smbh_positions, imbh_positions)
-    print("> Made plots")
-
-    np.savez("test.npz", smbh_pos=smbh_positions, imbh_pos=imbh_positions)
-    print("> Saved positions")
 
 
 if __name__ == "__main__":
