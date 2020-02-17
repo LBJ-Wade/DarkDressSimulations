6d2d5b85ca8855c2f6d66ac2fffddc5cb5d225b3
bradkav <bradley.kavanagh@cantab.net>
2020-02-17 15:24:34 +0100

    Minor change to units

--- initial_conditions/units.py
+++ initial_conditions/units.py
@@ -2,4 +2,4 @@ G_N = 4.3020077853e-3 #(pc/solar mass) (km/s)^2
 #G_N = 4.3018e-3 #(pc/solar mass) (km/s)^2
 #0.0043020077853
 C_LIGHT = 2.99792458e5 #km/s
-L_pc = 3.085678e18 #cm
+L_pc = 3.085678e13 #cm
